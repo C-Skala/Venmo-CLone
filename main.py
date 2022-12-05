015fb4f97530cac57d597a599417126c438ac1d5
C-Skala <cjskala@gmail.com>
2022-12-04 21:23:45 -0500

    made people

---
+++ main.py
@@ -0,0 +1,17 @@
+user_one = {
+'Full_name': 'Geralt Riverierre',
+'username': 'witcher',
+'password': 'yennifer',
+'account_balance': '10000',
+'connected_banks': 'yes',
+
+}
+
+user_two = {
+'Full_name': 'ask ketchum',
+'username': 'pokeMAN',
+'password': 'misty',
+'account_balance': '20',
+'connected_banks': 'yes',
+
+}
